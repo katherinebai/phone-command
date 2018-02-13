6391a0881627dcb74f9afe1872da52fc418065d6
katherinebai <36399340+katherinebai@users.noreply.github.com>
2018-02-13 16:53:42 +0800

    init phone command project

---
+++ PhoneCommand.cs
@@ -0,0 +1,51 @@
+﻿using GrapeCity.Forguncy.Commands;
+using GrapeCity.Forguncy.Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCommand
+{
+    [Icon("pack://application:,,,/PhoneCommand;component/Resources/Icon.png")]
+    public class PhoneCommand : Command
+    {
+        [DisplayName("提取号码单元格")]
+        [FormulaProperty(true)]
+        public object ExtractPhoneNumberCell { get; set; }
+
+        [DisplayName("提取策略")]
+        public ExtractPolicy ExtractPolicy { get; set; }
+
+        [DisplayName("提取号码类型")]
+        public ExtractPhoneType ExtractPhoneType { get; set; }
+
+        public override string ToString()
+        {
+            return "拨打电话命令";
+        }
+
+        public override CommandScope GetCommandScope()
+        {
+            return CommandScope.Cell | CommandScope.ListView | CommandScope.PageLoad;
+        }
+    }
+
+    public enum ExtractPolicy
+    {
+        [Description("匹配第一项")]
+        ExtractFirstOne,
+        [Description("匹配最后一项")]
+        ExtractLastOne
+    }
+
+    public enum ExtractPhoneType
+    {
+        [Description("手机号码")]
+        Mobile,
+        [Description("固定电话号码")]
+        Phone
+    }
+}
---
+++ Resources/PhoneCommand.js
@@ -0,0 +1,62 @@
+﻿var PhoneCommand = (function (_super) {
+    __extends(PhoneCommand, _super);
+    function PhoneCommand() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+
+    PhoneCommand.prototype.execute = function () {
+        // Get setings
+        var commandSettings = this.CommandParam;
+        var extractPolicy = commandSettings.ExtractPolicy;
+        var extractPhoneNumberCell = commandSettings.ExtractPhoneNumberCell;
+        var extractPhoneType = commandSettings.ExtractPhoneType;
+
+        var text = this.evaluateFormula(extractPhoneNumberCell);
+
+        var mobileNumbersArray = [];
+        var phoneNumbersArray = [];
+        if (extractPhoneType === ExtractPhoneType.Mobile) {
+            var mobileNumberMatches = text.match(/\d{3}\s*\d{4}\s*\d{4}/g);
+            if (mobileNumberMatches && mobileNumberMatches.length > 0) {
+                mobileNumberMatches.map(function (temp) {
+                    mobileNumbersArray.push(temp);
+                });
+            }
+        } else if (extractPhoneType === ExtractPhoneType.Phone) {
+            var phoneNumberMatches = text.match(/((\+?\s*\d{2,3}\s*\-?\s*)?(\d{2,3})\s*-?\s*)(\d{7,8})(\s*-?\s*(\d{3,}))?/g);
+            if (phoneNumberMatches && phoneNumberMatches.length > 0) {
+                phoneNumberMatches.map(function (temp) {
+                    temp = temp.replace(/\-/g, "");
+                    phoneNumbersArray.push(temp);
+                });
+            }
+        }
+
+        var array = mobileNumbersArray;
+        if (extractPhoneType === ExtractPhoneType.Phone) {
+            array = phoneNumbersArray;
+        }
+        if (array.length > 0) {
+            if (extractPolicy === ExtractPolicy.ExtractFirstOne) {
+                window.location.href = "TEL:" + array[0];
+            } else if (extractPolicy === ExtractPolicy.ExtractLastOne) {
+                window.location.href = "TEL:" + array[array.length - 1];
+            }
+        }
+    }
+
+    return PhoneCommand;
+}(Forguncy.CommandBase));
+
+var ExtractPolicy = {
+    ExtractFirstOne: 0,
+    ExtractLastOne: 1
+};
+
+var ExtractPhoneType = {
+    Mobile: 0,
+    Phone: 1
+}
+
+// Key format is "Namespace.ClassName, AssemblyName"
+Forguncy.CommandFactory.registerCommand("PhoneCommand.PhoneCommand, PhoneCommand", PhoneCommand);
